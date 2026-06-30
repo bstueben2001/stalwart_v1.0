7cc6b71e087828846a05dd3fc21cbe24c60c84c8
Brendon Stueben <bstueben2001@gmail.com>
2026-06-30 04:43:16 -0600

    added server code

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const path = require('path')
 
 const cors = require('cors');
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "dist")))
 
 // Routes
 const healthRouter = require('./Routes/healthRouter');
@@ -28,6 +30,9 @@ mongoose
   .connect(process.env.MONGO_URI, { serverSelectionTimeoutMS: 5000 })
   .then(() => {
     console.log('Connected to MongoDB');
+
+    app.get("*", (req, res) => res.sendFile(path.join(__dirname, "client", "dist", "index.html")))
+    
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
