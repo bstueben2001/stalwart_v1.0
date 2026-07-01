b56c8a7df745bfb9e01d0998264a55136e742275
Brendon Stueben <bstueben2001@gmail.com>
2026-07-01 06:40:54 -0600

    Home login routing adjustment

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -1,16 +1,25 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useAppContext } from '../Context';
 
 function Home() {
-  const { openAuth } = useAppContext();
+  const { user, openAuth } = useAppContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) navigate('/council');
+  }, [user, navigate]);
 
   return (
     <>
       <main className="home-page">
         <h1>Welcome to Stalwart</h1>
         <p>Your personal council of advisors awaits.</p>
-        <button className="home-begin-btn" onClick={() => openAuth('login')}>
-          Begin
-        </button>
+        {!user && (
+          <button className="home-begin-btn" onClick={() => openAuth('login')}>
+            Begin
+          </button>
+        )}
       </main>
       <img src="/knight.png" alt="Knight" className="home-knight" />
     </>
